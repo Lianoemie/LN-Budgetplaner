7ba18816e62e5c7f1f7f705722660bd75477b748
Elena Stevanovic <stevaele@students.zhaw.ch>
2025-05-07 11:07:47 +0200

    rausgenommen

--- Start.py
+++ Start.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-# ====== Start Login Block ======
-from utils.login_manager import LoginManager
-LoginManager().go_to_login('Start.py') 
-# ====== End Login Block ======
-
 from utils.data_manager import DataManager
 
 # Initialize the data manager
