7065c78780040b563af99887a651dda590d9f334
Elena Stevanovic <stevaele@students.zhaw.ch>
2025-05-11 14:08:12 +0200

    Versuch Login-Manager

--- Start.py
+++ Start.py
@@ -7,11 +7,16 @@ from utils.login_manager import LoginManager
 # Initialize the data manager
 data_manager = DataManager(fs_protocol='webdav', fs_root_folder="Studibudget")
 
-if 'data_df' not in st.session_state:
-    data_manager.load_user_data(
-        session_state_key='data_df', 
-        file_name='data.csv', 
-        initial_value=pd.DataFrame()
+# initialize the login manager
+login_manager = LoginManager(data_manager)
+login_manager.login_register()  # open login/register page
+
+# load the data from the persistent storage into the session state
+data_manager.load_user_data(
+    session_state_key='data_df', 
+    file_name='data.csv', 
+    initial_value = pd.DataFrame(), 
+    parse_dates = ['timestamp']
     )
 
 # --- Welcome Page ---
