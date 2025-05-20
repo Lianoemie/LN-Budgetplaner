d0467752c5a5f2001784f9b00b742d3d7d88f025
SelinaPat <ruedisel@students.zhaw.ch>
2025-05-20 20:47:35 +0200

    mal schaun

--- pages/00_Titelbild.py
+++ pages/00_Titelbild.py
@@ -1,15 +1,12 @@
 import streamlit as st
 from PIL import Image
 
-# Seite konfigurieren (muss ganz oben stehen)
-st.set_page_config(page_title="Titelseite", page_icon="🎓")
-
 # Optionaler rosa Hintergrund
 from utils.style import set_background
 set_background()
 
 # Titelbild anzeigen
-image = Image.open("Fotos/Titelbild.png")  # Pfad anpassen falls nötig
+image = Image.open("docs/Fotos/Titelbild.png")  # Pfad anpassen falls nötig
 st.image(image, use_column_width=True)
 
 # Optional: etwas Abstand oder Begrüßung
