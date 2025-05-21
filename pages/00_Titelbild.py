e3de921ed41d0b252ccbed0804287aa6c5dc5f84
SelinaPat <ruedisel@students.zhaw.ch>
2025-05-21 11:02:11 +0200

    lottie weg

--- pages/00_Titelbild.py
+++ pages/00_Titelbild.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_lottie import st_lottie
 import requests
 from PIL import Image
 import time
@@ -10,18 +9,6 @@ set_background() # Hintergrundfarbe anzeigen
 # Titelbild anzeigen
 bild = Image.open("docs/Fotos/Titelbild.png")
 st.image(bild, use_container_width=True)
-
-# Lottie-Animation laden
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-# Beispiel: Finanz-Animation
-lottie_animation = load_lottieurl("https://assets2.lottiefiles.com/packages/lf20_qp1q7mct.json")
-
-st_lottie(lottie_animation, speed=1, reverse=False, loop=True, quality="low", height=300)
 
 
 # Schriftzug mit Typewriter-Effekt
